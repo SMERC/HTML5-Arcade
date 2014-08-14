dbd82d76faeaa0e53548a22cf71be8c4613444a3
Diego Machado <tuxvader@gmail.com>
2014-08-14 16:47:18 -0300

    Added events

---
+++ js/index.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Diego on 8/14/14.
+ */
+
+$(document).ready(function(){
+
+    var mFirstGameLoad = false;
+
+    function onGameSelected(aElement){
+        var aGameIframe = $('#game_iframe');
+        aGameIframe.attr("src", aElement.attr('game-url'));
+
+        if(mFirstGameLoad == false){
+            mFirstGameLoad = true;
+            aGameIframe.animate({height:640}, 500);
+        }
+    }
+
+    var aSelectableGames = $('.selectable_game');
+
+    aSelectableGames.each(function(){
+        Tools.addTouchEvents($(this), onGameSelected, $(this));
+    });
+
+});
